219aade17a4a74a7aa482ec5b6ba1d2575f17557
LucaSpennato <luca.spennato@gmail.com>
2022-06-23 19:19:30 +0200

    function on bonus added

--- js/main.js
+++ js/main.js
@@ -12,74 +12,90 @@
 // con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
 // * Salvo il bottone in variabile
-    // ? lavorando sul click, allora tutto si svilupperà nel bottone, tranne le funzioni da richiamare
+// ? lavorando sul click, allora tutto si svilupperà nel bottone, tranne le funzioni da richiamare
 const playButton = document.getElementById('start_btn');
 
-playButton.addEventListener('click', function(){  // TODO Lo useremo alla fine per avere tutto all'avvio del documento per ora!
+playButton.addEventListener('click', function () {  // TODO Lo useremo alla fine per avere tutto all'avvio del documento per ora!
 
-// Richiamo il wrapper per poter inserir le cose dentro successivamente in modo più comodo
-const gameWrapper = document.querySelector('.grid_wrapper');
-gameWrapper.innerHTML = '';
+    // Richiamo il wrapper per poter inserir le cose dentro successivamente in modo più comodo
+    const gameWrapper = document.querySelector('.grid_wrapper');
+    gameWrapper.innerHTML = '';
 
-// ! Bonus
-// * Creo un value selector su html con le varie difficoltà
-// * recupero il value attributo al selettore su js, come lo modifico?
-let difficultySelector = document.getElementById('difficulty_selector');
+
+    let difficultySelector = document.getElementById('difficulty_selector');
+
+    // ! Bonus SOTTO LE FUNZIONI!
+    // * Creo un value selector su html con le varie difficoltà
+    // * recupero il value attributo al selettore su js, come lo modifico?
     // ? assegno a difficulty selector il value assegnato
-    difficultySelector = parseInt(difficultySelector.value);
-    let diffForIteration = difficultySelector;
-    let diffClassChange = difficultySelector;
-    // console.log(difficultySelector);
     // ? in base al value, cambio con un if i valori al for
-    if(difficultySelector === 1){
-        diffForIteration = 100;
-        diffClassChange = 'box_easy';
-    } else if (difficultySelector === 2){
-        diffForIteration = 81;
-        diffClassChange = 'box_medium';
-    } else if (difficultySelector === 3){
-        diffForIteration = 49;
-        diffClassChange = 'box_difficult';
-    } else {
-        diffForIteration = 0;
+    // ? creo delle nuove classi box da assegnare con le varie differenze
+
+    difficultySelector = parseInt(difficultySelector.value);
+    if (isNaN(difficultySelector)) {
         gameWrapper.innerHTML = '<div class="first_screen fs-1 text-light">Scegli prima una difficoltà!</div>';
     }
-    // ? creo delle nuove classi box da assegnare con le varie differenze
-    // TODO metter tutto in una o più funzioni! function difficultyIteration (){}
 
+    let diffForIteration = difficultyIteration(difficultySelector);
+    let diffClassChange = difficultyClassChange(difficultySelector);
 
-
-// * creo le 100 caselle dinamicamente su js:
+    // * creo le 100 caselle dinamicamente su js:
     //  ? creo un for per stampare le 100 caselle
-for (let i = 0; i < diffForIteration; i++) {
+    for (let i = 0; i < diffForIteration; i++) {
 
-    let gameGenerator = boxesGenerator(diffClassChange, 'borders');
-    // serve a far visualizzare da 1 a 100 sulla grigilia
-    gameGenerator.innerHTML = i +1;
-    gameWrapper.append(gameGenerator);
+        let gameGenerator = boxesGenerator(diffClassChange, 'borders');
+        // serve a far visualizzare da 1 a 100 sulla grigilia
+        gameGenerator.innerHTML = i + 1;
+        gameWrapper.append(gameGenerator);
 
-    activateBoxes(gameGenerator, 'active');
-}
-
+        activateBoxes(gameGenerator, 'active');
+    }
 })
 
 // ? la modulazione delle 100 caselle la assegnerò ad una funzione richiamandola poi nel for
-function boxesGenerator(classToAdd, classToAddTwo){
+function boxesGenerator(classToAdd, classToAddTwo) {
     let box = document.createElement('div');
     box.classList.add(classToAdd, classToAddTwo);
     return box;
 }
 
 // * Devo far attivare le caselle al click e cambia colore al click
-    // ?  creo una classe 'attivo' su css (già creata)
-    // ? creo una funzione che mi permetta al click, di cambiare la classe su js 
-function activateBoxes(elementPressed, effectToActivate){
-    
-    elementPressed.addEventListener('click', function(){
+// ?  creo una classe 'attivo' su css (già creata)
+// ? creo una funzione che mi permetta al click, di cambiare la classe su js 
+function activateBoxes(elementPressed, effectToActivate) {
+
+    elementPressed.addEventListener('click', function () {
         elementPressed.classList.add(effectToActivate);
 
-         // ? al click, mi darà anche il log con il numero della casella cliccata
+        // ? al click, mi darà anche il log con il numero della casella cliccata
         console.log(elementPressed.innerHTML);
     })
 
+}
+
+
+// ! Bonus
+function difficultyIteration(valueSelected) {
+    parseInt(valueSelected);
+    if (valueSelected === 1) {
+        return 100;
+    } else if (valueSelected === 2) {
+        return 81;
+    } else if (valueSelected === 3) {
+        return 49;
+    } else {
+        return 0;
+    }
+}
+function difficultyClassChange(valueSelected) {
+    parseInt(valueSelected);
+    if (valueSelected === 1) {
+        return 'box_easy';
+    } else if (valueSelected === 2) {
+        return 'box_medium';
+    } else if (valueSelected === 3) {
+        return 'box_difficult';
+    } else {
+        return 0;
+    }
 }

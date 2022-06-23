99c2633f9e853ef43a122a913b65e61e4b7f5321
LucaSpennato <luca.spennato@gmail.com>
2022-06-23 16:24:36 +0200

    boxes add to for and function

--- js/main.js
+++ js/main.js
@@ -13,12 +13,33 @@
 
 // * Salvo il bottone in variabile
     // ? lavorando sul click, allora tutto si svilupperà nel bottone, tranne le funzioni da richiamare
+const playButton = document.getElementById('start_btn');
+
+// TODO playButton.addEventListener('click', function(){ }) Lo useremo alla fine per avere tuto all'avvio del documento per ora!
+
+// Richiamo il wrapper per poter inserir le cose dentro successivamente in modo più comodo
+const gameWrapper = document.querySelector('.grid_wrapper');
+console.log(gameWrapper);
 
 // * creo le 100 caselle dinamicamente su js:
-    // ? creo un for per stampare le 100 caselle
+    //  ? creo un for per stampare le 100 caselle
     // ? la modulazione delle 100 caselle la assegnerò ad una funzione richiamandola poi nel for
+for (let i = 0; i < 100; i++) {
+
+    let gameGenerator = boxesGenerator();
+
+    gameGenerator.classList.add('box', 'borders');
+    gameWrapper.append(gameGenerator);
+}
+
+
+
 
 // * Devo far attivare le caselle al click e cambia colore al click
     // ?  creo una classe 'attivo' su css (già creata)
     // ? creo una funzione che mi permetta al click, di cambiare la classe su js 
     // ? al click, mi darà anche il log con il numero della casella cliccata
+
+function boxesGenerator(){
+    return document.createElement('div');
+}
